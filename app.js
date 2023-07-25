6732fdc6e1210fb0605dea3e466de22daeb1b4c9
santi142004 <santiagobarreralopez2004@gmail.com>
2023-07-25 09:56:33 -0500

    gifs actualizados

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
-const apiKey = 'AIzaSyDr-hfn4RaXZzUWBfdp5NtiPxOboacEwl0';
-const search = document.getElementById('search').value;
-const url = `https://api.tenor.com/v1/search?q=${encodeURIComponent(search)}&key=${apiKey}`;
+const apiKey = ' AIzaSyDr-hfn4RaXZzUWBfdp5NtiPxOboacEwl0';
+const search = document.getElementById('search');
+
+search.addEventListener('keydown', function(event){
+
+  if (event.keyCode == 13) {
+    const word=search.value
+    console.log(word);
+  }
+})
+
+const url = `https://tenor.googleapis.com/v2/search?q=${(search)}&key=${apiKey}`;
 const gifs1= document.getElementById('trendGif')
 
-fetch(url)
+function objects(url) {
+  fetch(url)
   .then(response => response.json())
-  .then(data => {
-    // Procesar la respuesta JSON para obtener los GIFs
-    const gifs = data.results.map(result => result.media[0].gif.url);
+  .then(data => console.log(data))
+  .catch(err => console.error(err));  
+}
 
-    // Mostrar los GIFs en tu aplicación o sitio web
-    gifs.forEach(gifUrl => {
-      const img = document.createElement('img');
-      img.src = gifUrl;
-      gifs1.appendChild(img);
-    });
-  })
-  .catch(error => {
-    console.error('Error al obtener los GIFs:', error);
-  });
+objects(url)
+
